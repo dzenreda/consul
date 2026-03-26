consuldemocracy/consuldemocracy

Use the default view mode icon by default
In the `default_view_mode?` helper we assume that the view mode is the
default one unless the minimal mode is explicitly specified. So now
we're applying the same logic to the CSS. This is usually how the
cascade works: you define a default style, and then overwrite it with a
specific one.

This means we no longer need to specify the `default` HTML class when
the view mode is the default one.

spec/components/shared/view_mode_component_spec.rb
@@ -2,21 +2,23 @@ require "rails_helper"
 
 describe Shared::ViewModeComponent do
   describe "main element" do
-    it "uses a default class by default" do
+    it "uses no specific view mode class by default" do
       with_request_url("/example") do
         render_inline Shared::ViewModeComponent.new
 
-        expect(page).to have_css ".view-mode.default"
+        expect(page).to have_css ".view-mode"
         expect(page).not_to have_css ".view-mode.minimal"
+        expect(page).not_to have_css ".view-mode.default"
       end
     end
 
-    it "uses a default class when the defaut mode is specified" do
+    it "uses no specific view mode class when the defaut mode is specified" do
       with_request_url("/example?view=default") do
         render_inline Shared::ViewModeComponent.new
 
-        expect(page).to have_css ".view-mode.default"
+        expect(page).to have_css ".view-mode"
         expect(page).not_to have_css ".view-mode.minimal"
+        expect(page).not_to have_css ".view-mode.default"
       end
     end
 

dzenreda/consul

spec/components/shared/view_mode_component_spec.rb
@@ -2,21 +2,23 @@ require "rails_helper"
 
 describe Shared::ViewModeComponent do
   describe "main element" do
-    it "uses a default class by default" do
+    it "uses no specific view mode class by default" do
       with_request_url("/example") do
         render_inline Shared::ViewModeComponent.new
 
-        expect(page).to have_css ".view-mode.default"
+        expect(page).to have_css ".view-mode"
         expect(page).not_to have_css ".view-mode.minimal"
+        expect(page).not_to have_css ".view-mode.default"
       end
     end
 
-    it "uses a default class when the default mode is specified" do
+    it "uses no specific view mode class when the default mode is specified" do
       with_request_url("/example?view=default") do
         render_inline Shared::ViewModeComponent.new
 
-        expect(page).to have_css ".view-mode.default"
+        expect(page).to have_css ".view-mode"
         expect(page).not_to have_css ".view-mode.minimal"
+        expect(page).not_to have_css ".view-mode.default"
       end
     end
 