86d62091ca3dc98874484f50d2708fdb679475d0
Diego Calvo <diego.calvo@enreda.coop>
2026-04-01 18:30:04 +0200

    CSV export for debates

---
+++ app/models/debate/exporter.rb
@@ -0,0 +1,31 @@
+class Debate::Exporter
+  include CsvExporter
+
+  attr_reader :records
+
+  def initialize(debates)
+    @records = debates
+  end
+
+  def model
+    Debate
+  end
+
+  private
+
+    def headers
+      [
+        I18n.t("admin.debates.index.id"),
+        Debate.human_attribute_name(:title),
+        I18n.t("admin.debates.index.author")
+      ]
+    end
+
+    def csv_values(debate)
+      [
+        debate.id.to_s,
+        debate.title,
+        debate.author.email
+      ]
+    end
+end
